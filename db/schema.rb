c06cd122fccf83a2ae12467e75f926a96db34847
Shelah Horvitz <shelah@shelahhorvitz.com>
2015-08-08 15:42:54 -0400

    Got primary id autoincrementing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808160850) do
+ActiveRecord::Schema.define(version: 20150808191249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20150808160850) do
   create_table "crops", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
+
+  add_index "crops", ["name"], name: "index_crops_on_name", unique: true, using: :btree
 
 end
--- spec/models/crop_spec.rb
+++ spec/models/crop_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Crop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'verify contents' do
+    context 'creation without name' do
+      it 'should fail creation' do
+        expect { Crop.create!.to fail }
+      end
+    end
+
+    context 'creation with name' do
+      it 'should successfully create' do
+        expect { Crop.create!(:name => 'Apple').to succeed }
+      end
+    end
+  end
 end
